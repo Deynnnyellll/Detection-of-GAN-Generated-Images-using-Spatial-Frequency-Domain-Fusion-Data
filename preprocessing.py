f1177a0304f4a9851a85ea2c68802e717c7364e7
Matthew-Gallardo <106909575+Matthew-Gallardo@users.noreply.github.com>
2023-10-13 09:25:12 +0800

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,8 @@ going to feature extraction phase
 1. The image will be converted to grayscale
 2. The image will be resize to 256x256 (as mentioned to the system architecture
    where images will be resize to a common size)
+3. The image will have a noice reduction using Gaussian Noice Reduction also known as Gaussian blur or Gaussian smoothing
+
 '''
 
 import cv2
@@ -12,12 +14,14 @@ import numpy as np
 import time
 
 def preprocessing(image):
+    # Load the image in grayscale
     image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
+    # Check the dimensions
     dimension = np.array(image.shape)
 
     if dimension[0] == 256 and dimension[1] == 256:
-        print("\n... tImage is already in 256x256\n\n")
+        print("\n... Image is already in 256x256\n\n")
         final_image = image
     else:
         print("\nWarning: Image is not resized to 256x256")
@@ -28,6 +32,10 @@ def preprocessing(image):
         print('...')
         time.sleep(1)
         final_image = cv2.resize(image, (256, 256))
+
+    # Apply Gaussian noise reduction
+    print("\nPerforming Gaussian noise reduction")
+    final_image = cv2.GaussianBlur(final_image, (5, 5), 0)  # You can adjust the kernel size and standard deviation as needed
 
     time.sleep(2)
     return final_image
