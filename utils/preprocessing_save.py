b33a728381036899d26a80fec5ab1bccf03fd196
Deynnnyellll <acadsnidanniel04@gmail.com>
2023-11-03 14:04:50 +0800

    train_test jupyter notebook

--- utils/preprocessing_save.py
+++ utils/preprocessing_save.py
@@ -36,18 +36,3 @@ def save_image(image_list, directory):
     if directory is not None:
         print(f"\n------------------{len(image_list)} Images converted successfully-------------------\n")    
 
-
-# directory for reading
-img_real = "/Users/Danniel/Downloads/real"
-img_gan = "/Users/Danniel/Downloads/gan"
-
-# directory for saving (must be an empty folder)
-save_dir_real = "/Users/Danniel/Downloads/preprocessed_real"
-save_dir_gan = "/Users/Danniel/Downloads/preprocessed_gan"
-
-# store the images
-image_real = load_image(img_real)
-image_gan = load_image(img_gan)
-
-save_image(image_real, save_dir_real)
-save_image(image_gan, save_dir_gan)
